bb4173d2f2b776333146d7fbccac282aabca3bf4
Johannes Waigel <johannes@waigel.me>
2024-09-08 20:57:31 +0000

    add example

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,36 @@
-import { Client } from "pg";
+import { Client } from 'pg';
 
 export interface Env {
-  // If you set another name in wrangler.toml as the value for 'binding',
-  // replace "HYPERDRIVE" with the variable name you defined.
-  HYPERDRIVE: Hyperdrive;
+	// If you set another name in wrangler.toml as the value for 'binding',
+	// replace "HYPERDRIVE" with the variable name you defined.
+	MY_HYPERDRIVE: Hyperdrive;
 }
 
 export default {
-  async fetch(request, env, ctx): Promise<Response> {
-    const client = new Client({
-      connectionString: c.env.HYPERDRIVE.connectionString,
-    });
+	async fetch(request, env, ctx): Promise<Response> {
+		const client = new Client({
+			connectionString: env.MY_HYPERDRIVE.connectionString,
+		});
 
-    try {
-      // Connect to your database
-      await client.connect();
+		try {
+			// Connect to your database
+			await client.connect();
 
-      // A very simple test query
-      const result = await client.query({ text: "SELECT * FROM pg_tables" });
+			// A very simple test query
+			const result = await client.query({ text: 'SELECT * FROM pg_tables' });
 
-      // Clean up the client, ensuring we don't kill the worker before that is
-      // completed.
-      ctx.waitUntil(client.end());
+			// Clean up the client, ensuring we don't kill the worker before that is
+			// completed.
+			ctx.waitUntil(client.end());
 
-      // Return result rows as JSON
-      return Response.json({ result: result });
-    } catch (e) {
-      console.log(e);
-      return Response.json({ error: e.message }, { status: 500 });
-    }
-  },
+			// Return result rows as JSON
+			return Response.json({ result: result });
+		} catch (e) {
+			console.log(e);
+			if (e instanceof Error) {
+				return Response.json({ error: e.message }, { status: 500 });
+			}
+			return Response.json({ error: 'Unknown error' }, { status: 500 });
+		}
+	},
 } satisfies ExportedHandler<Env>;
