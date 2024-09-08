810f1eae83b88f2666f76ead69c9b85bd28afec1
Johannes Waigel <johannes@waigel.me>
2024-09-08 20:42:47 +0000

    Initial commit (by create-cloudflare CLI)
    Details:
      C3 = create-cloudflare@2.26.0
      project name = example-pg-cloudflare
      package manager = pnpm@9.7.1
      wrangler = wrangler@3.73.0
      git = 2.46.0

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { Client } from "pg";
+
+export interface Env {
+  // If you set another name in wrangler.toml as the value for 'binding',
+  // replace "HYPERDRIVE" with the variable name you defined.
+  HYPERDRIVE: Hyperdrive;
+}
+
+export default {
+  async fetch(request, env, ctx): Promise<Response> {
+    const client = new Client({
+      connectionString: c.env.HYPERDRIVE.connectionString,
+    });
+
+    try {
+      // Connect to your database
+      await client.connect();
+
+      // A very simple test query
+      const result = await client.query({ text: "SELECT * FROM pg_tables" });
+
+      // Clean up the client, ensuring we don't kill the worker before that is
+      // completed.
+      ctx.waitUntil(client.end());
+
+      // Return result rows as JSON
+      return Response.json({ result: result });
+    } catch (e) {
+      console.log(e);
+      return Response.json({ error: e.message }, { status: 500 });
+    }
+  },
+} satisfies ExportedHandler<Env>;
